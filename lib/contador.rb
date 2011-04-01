758cd71ebb8a13e7e2f6e962a2946249264f7275
Ivan Acosta-Rubio <ivan@bakedweb.net>
2011-03-31 23:42:59 -0600

    omiting some words when needed

--- lib/contador.rb
+++ lib/contador.rb
@@ -1,32 +1,44 @@
-class Contador 
+class Contador
 
   attr_accessor :string
 
-  def initialize(string) 
+  def initialize(string)
+    @omit = []
     @string = string
     @final = Hash.new
     split_string_into_hash
+    delete_omits if $omits
   end
 
-  def multiplicity 
+  def delete_omits
+    $omits.split(' ').each do |word|
+      @final.delete(word.to_sym)
+    end
+  end
+
+  def multiplicity
     @final
+  end
+
+  def omit(word)
+    @final.delete(word.to_sym)
   end
 
   private
 
-  def split_string_into_hash 
+  def split_string_into_hash
     string.split(' ').each do |word|
       begin
-        word.gsub!(/\W+/, '') 
+        word.gsub!(/\W+/, '')
         key = word.downcase.to_sym
         if @final.has_key?(key)
-          @final[key] += 1  
+          @final[key] += 1
         else
           @final[key] = 1
         end
       rescue
         puts "Upsss.. #{word}"
       end
-      end
+    end
   end
 end
